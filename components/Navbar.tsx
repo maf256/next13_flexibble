056cc63760f950d7aec8e6f17c527ca4bbbce42c
maf256 <maf256@gmail.com>
2023-07-18 15:27:27 +0200

    Added constant and Working on Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,44 @@
+import { NavLinks } from '@/constant'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
+    const session = {}
+    return (
+        <nav className='flexBetween navbar'>
+            <div className='flex-1 flexStart gap-10'>
+                <Link href='/'>
+                    <Image
+                        src='/logo.svg'
+                        width={115}
+                        height={43}
+                        alt='Flexibble'
+                    />
+                </Link>
+                <ul className='xl:flex hidden text-smal gap-7'>
+                    {NavLinks.map((link) => (
+                        <Link href={link.href} key={link.key}>
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+            <div className='flexCenter gap-4'> 
+                {session ? (
+                    <>
+                        UserPhoto
+                        <Link href='/create-project'>
+                            Share Work
+                        </Link>
+                    </>
+                ) : (
+                    <>
+                    </>
+                )}
+            </div>
+        </nav>
+    )
 }
 
 export default Navbar
