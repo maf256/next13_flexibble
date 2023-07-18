464bf495f228d7f85002b39cd8082eefcc57af5a
maf256 <maf256@gmail.com>
2023-07-18 19:41:58 +0200

    install grafbase

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,16 @@
+import { g, auth, config } from '@grafbase/sdk'
+
+
+
+
+export default config({
+  schema: g
+  // Integrate Auth
+  // https://grafbase.com/docs/auth
+  // auth: {
+  //   providers: [authProvider],
+  //   rules: (rules) => {
+  //     rules.private()
+  //   }
+  // }
+})
