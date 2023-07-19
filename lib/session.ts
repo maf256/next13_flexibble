81b5889c0964b59505075b0fe4cfdc321e63ba5d
maf256 <maf256@gmail.com>
2023-07-20 00:34:41 +0200

    uodate .env working in session

--- lib/session.ts
+++ lib/session.ts
@@ -4,32 +4,39 @@ import { AdapterUser } from "next-auth/adapters";
 import GoogleProvider from 'next-auth/providers/google'
 import jsonwebToken from 'jsonwebtoken'
 import { JWT } from 'next-auth/jwt'
+import { log } from "console";
 
 export const authOptions: NextAuthOptions = {
     providers: [
         GoogleProvider({
-            clientId: '',
-            clientSecret: ''
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!
         })
     ],
-    jwt: {
-        encode: ({secret, token}) =>{
+    // jwt: {
+    //     encode: ({secret, token}) =>{
 
-        }
-        decode: async({secret, token}) =>{
+    //     }
+    //     decode: async({secret, token}) =>{
             
-        }
-    },
+    //     }
+    // },
     theme: {
         colorScheme: 'light',
         logo: '/logo.png'
     },
     callbacks: {
         async session({ session }) {
+            return session
 
         },
-        async signIn({ user }) {
-            
+        async signIn({ user } : { user: AdapterUser | User}) {
+            try {
+                return true
+             }catch(err: any) {
+                console.log(err)
+                return false
+            }
         }
     }
 }
