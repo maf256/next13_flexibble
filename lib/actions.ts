a35a5be7ffddb5e9ee60e9e890be4d1149a5f280
maf256 <maf256@gmail.com>
2023-07-21 17:09:18 +0200

    Create actions.ts

---
+++ lib/actions.ts
@@ -0,0 +1,19 @@
+import { GraphQLClient } from "graphql-request";
+import { createProjectMutation, createUserMutation, deleteProjectMutation, updateProjectMutation, getProjectByIdQuery, getProjectsOfUserQuery, getUserQuery, projectsQuery } from "@/graphql";
+import { Query } from "@grafbase/sdk/dist/src/query";
+
+const isProduction = process.env.NODE_ENV === 'production'
+
+const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql'
+const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_key || '' : 'letmen'
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000'
+
+const client = new GraphQLClient(apiUrl) 
+
+const makeGraphQLRequest = async (query: string, variables = {}) => {
+    try{
+
+    }catch(error){
+        throw error
+    }
+} 
